419f4ad35921666cc1717872e5498614d3f5548a
daniel wang <daniel867181@gmail.com>
2022-03-08 18:18:53 +1100

    updated

--- web-scraper.py
+++ web-scraper.py
@@ -1,16 +1,16 @@
 import bs4
-from urllib.request import urlopen as uReq
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as soup
 
 my_url = 'https://www.who.int/emergencies/disease-outbreak-news'
 
 # open a connection and grab the page
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+req = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+page_html = urlopen(req).read()
 
 # parse the html
 page_soup = soup(page_html, "html.parser")
 
 # TODO:find a class to grab the disease info
-containers = page_soup.findAll("div", {"class":"lister-item-content"})
+titles = page_soup.findAll("span", {"class":"trimmed"})
+print(titles)
