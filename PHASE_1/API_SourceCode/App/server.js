00707a83f89a3d97e059c811e867cbad026cb8ff
rexanaj <66309802+rexanaj@users.noreply.github.com>
2022-03-15 21:17:32 +1100

    Issue/dwen 39 setup database (#5)
    * Set up Firebase and added example 'dbTest' route
    
    * Re-added in PORT env var to test Heroku deployment
    
    * Updated 'require' statements to pass tests
    
    * Added in Github repo secrets & fixed Github testing workflow
    
    * Update Swagger route and port

--- PHASE_1/API_SourceCode/App/server.js
+++ PHASE_1/API_SourceCode/App/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5555;
 
+// Begins the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
