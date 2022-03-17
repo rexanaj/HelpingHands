2b50e2a41fd358307cda920a8f9a3c4435fcc824
thao <reiin744@gmail.com>
2022-03-18 00:01:16 +1100

    Add tests for limit parameter

--- PHASE_1/API_SourceCode/App/api/routes/routes.test.js
+++ PHASE_1/API_SourceCode/App/api/routes/routes.test.js
@@ -17,10 +17,48 @@ describe("Routes", () => {
     expect(res.body).toEqual("Invalid parameter type");
   });
 
-  it("GET /articles/limit=100 ==> successful", async () => {
-    const res = await supertest(app).get("/articles?limit=100");
+  it("GET /articles/limit=1 ==> successful", async () => {
+    const res = await supertest(app).get("/articles?limit=1");
 
+    //check that the first article in the database is in the response
+    const expected = { "id":1 };
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual("Success");
+    expect(res.body[0]).toMatchObject(expected);
+
+    //check that only one article is in the response
+    expect(res.body).toHaveLength(1);
+  });
+
+  it("GET /articles/limit=3 ==> successful", async () => {
+    const res = await supertest(app).get("/articles?limit=3");
+
+    //check that the first three articles in the database are in the response
+    const expected_1 = { "id":1 };
+    const expected_2 = { "id":2 };
+    const expected_3 = { "id":3 };
+    expect(res.statusCode).toEqual(200);
+    expect(res.body[0]).toMatchObject(expected_1);
+    expect(res.body[1]).toMatchObject(expected_2);
+    expect(res.body[2]).toMatchObject(expected_3);
+
+    //check that three article are in the response
+    expect(res.body).toHaveLength(3);
+  });
+
+  it("GET /articles/limit=10 ==> successful", async () => {
+    const res = await supertest(app).get("/articles?limit=10");
+
+    //when there are less articles in the database than the limit query,
+    //return the first three articles
+    const expected_1 = { "id":1 };
+    const expected_2 = { "id":2 };
+    const expected_3 = { "id":3 };
+    expect(res.statusCode).toEqual(200);
+    expect(res.body[0]).toMatchObject(expected_1);
+    expect(res.body[1]).toMatchObject(expected_2);
+    expect(res.body[2]).toMatchObject(expected_3);
+
+    //check that three article are in the response
+    expect(res.body).toHaveLength(3);
   });
 });
