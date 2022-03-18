0c6bd5d0cfdc545099f731cba6d0fd9d095b9498
Shangjie Zhou <zhoujay73@gmail.com>
2022-03-18 14:24:41 +0800

    disease testing done

--- PHASE_1/API_SourceCode/App/api/routes/routes.test.js
+++ PHASE_1/API_SourceCode/App/api/routes/routes.test.js
@@ -54,26 +54,28 @@ describe("Testing all diseases info", () => {
 });
 
 describe("Testing specified disease name", () => {
-  it("GET /:disease ==> return info for the specified disease", async () => {
-    const testDiseaseName = diseaseNames[0];
-    const res = await supertest(app).get("/diseases/" + testDiseaseName);
+  it("GET /:disease ==> return info for the specified disease", () => {
+    diseaseNames.forEach(async n => {
+      const testDiseaseName = n;
+      const res = await supertest(app).get("/diseases/" + testDiseaseName);
 
-    expect(res.statusCode).toEqual(200);
-    const disease = res.body;
-    // check if the fetched the disease is the same as the passed in disease
-    expect(disease.disease.toUpperCase()).toEqual(testDiseaseName);
-    // check if locations exist
-    expect(disease.locations).toEqual(expect.anything());
-    // check if syndromes are valid
-    expect(syndromes).toEqual(expect.arrayContaining(disease.syndromes));
-    // check if the number of cases is a valid number
-    expect(disease.number_of_cases).toBeGreaterThan(0);
-    // check if log exists
-    expect(disease.log).toEqual(expect.anything());
+      expect(res.statusCode).toEqual(200);
+      const disease = res.body;
+      // check if the fetched the disease is the same as the passed in disease
+      expect(disease.disease.toUpperCase()).toEqual(testDiseaseName);
+      // check if locations exist
+      expect(disease.locations).toEqual(expect.anything());
+      // check if syndromes are valid
+      expect(syndromes).toEqual(expect.arrayContaining(disease.syndromes));
+      // check if the number of cases is a valid number
+      expect(disease.number_of_cases).toBeGreaterThanOrEqual(0);
+      // check if log exists
+      expect(disease.log).toEqual(expect.anything());
+    })
   });
 
-  it("GET /:disease(invalid disease name) ==> return 400", async () => {
+  it("GET /:disease(invalid disease name) ==> return 404", async () => {
     const res = await supertest(app).get("/diseases/some-invalid-disease");
-    expect(res.statusCode).toEqual(400);
+    expect(res.statusCode).toEqual(404);
   });
 });
