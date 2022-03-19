41aa702d15c7837792213937843e432442718713
thao <reiin744@gmail.com>
2022-03-19 14:49:58 +1100

    Add tests for start date parameter

--- PHASE_1/API_SourceCode/App/api/routes/routes.test.js
+++ PHASE_1/API_SourceCode/App/api/routes/routes.test.js
@@ -8,7 +8,6 @@ describe("Routes", () => {
   //Check correct format of start date (missing month and year)
   it("GET /articles/limit=1/start_date=29 ==> Invalid start date", async () => {
     const res = await supertest(app).get("/articles?limit=1&start_date=29");
-
     expect(res.statusCode).toEqual(400);
     expect(res.body).toEqual("Invalid start date");
   });
@@ -16,53 +15,55 @@ describe("Routes", () => {
   //Check correct format of start date (missing year)
   it("GET /articles/limit=1/start_date=29- ==> Invalid start date", async () => {
     const res = await supertest(app).get("/articles?limit=1&start_date=30-02");
-
     expect(res.statusCode).toEqual(400);
     expect(res.body).toEqual("Invalid start date");
   });
 
   //Check correct articles are returned with respect to start date
   it("GET /articles/limit=1/start_date=02-03-2022 ==> successful", async () => {
-    const res = await supertest(app).get("/articles?limit=1&start_date=2022-03-02");
+    const res = await supertest(app).get("/articles?limit=10&start_date=2022-02-01");
+    //five articles in the database have dates later than provided start date
 
-    //check that the correct article is in the response
-    const expected = { "date_of_publication": "3 March 2022" };
-    expect(res.body[0]).toMatchObject(expected);
-
-    expect(res.statusCode).toEqual(200);
-    //check that only one article is in the response
-    expect(res.body).toHaveLength(1);
-  });
-
-  //Check correct articles are returned with respect to start date
-  it("GET /articles/limit=1/start_date=02-03-2022 ==> successful", async () => {
-    const res = await supertest(app).get("/articles?limit=4&start_date=2022-02-01");
-
-    //check that the correct article is in the response
-    const expected_1 = { "date_of_publication": "3 March 2022" };
-    const expected_2 = { "date_of_publication": "21 February 2022" };
-    expect(res.body[0]).toMatchObject(expected);
-    expect(res.body[2]).toMatchObject(expected);
+    //convert start date query into unix timestamp
+    var unixStartDate = Date.parse("01-Feb-2022 00:00:00")/1000;
+    //compare unix time in articles to start date unix time
+    res.body.forEach(function (item, index) {
+      expect(item["date_of_publication"]["_seconds"]).toBeGreaterThanOrEqual(unixStartDate);
+    });
 
     expect(res.statusCode).toEqual(200);
-    //check that only one article is in the response
-    expect(res.body).toHaveLength(2);
+    //check that five articles are in the response
+    expect(res.body).toHaveLength(5);
   });
 
   //Tests for limit parameter
   //Test error codes
+  //no limit parameter
   it("GET /articles ==> missing limit parameter", async () => {
     const res = await supertest(app).get("/articles");
-
     expect(res.statusCode).toEqual(400);
     expect(res.body).toEqual("Missing limit parameter");
   });
 
+  //string passed into limit paramater
   it("GET /articles/limit=hi ==> invalid parameter type", async () => {
     const res = await supertest(app).get("/articles?limit=hi");
-
     expect(res.statusCode).toEqual(400);
     expect(res.body).toEqual("Invalid parameter type");
+  });
+
+  //limit equals 0
+  it("GET /articles/limit=0 ==> invalid parameter type", async () => {
+    const res = await supertest(app).get("/articles?limit=0");
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual("Invalid parameter value");
+  });
+
+  //limit is a negative number
+  it("GET /articles/limit=-5 ==> invalid parameter type", async () => {
+    const res = await supertest(app).get("/articles?limit=-5");
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual("Invalid parameter value");
   });
 
   //Check that content of response is correct and the right number
@@ -78,13 +79,5 @@ describe("Routes", () => {
     expect(res.statusCode).toEqual(200);
     //check that three article are in the response
     expect(res.body).toHaveLength(3);
-  });
-
-  it("GET /articles/limit=10 ==> successful", async () => {
-    const res = await supertest(app).get("/articles?limit=10");
-    expect(res.statusCode).toEqual(200);
-
-    //check that 10 article are in the response
-    expect(res.body).toHaveLength(10);
   });
 });
