a3a696e4da2cc9d519459e35d0f6de4ec8db5cfc
rexanaj <66309802+rexanaj@users.noreply.github.com>
2022-04-01 16:23:48 +1100

    Implemented /ngo route

--- PHASE_1/API_SourceCode/App/api/controllers/ngoController.js
+++ PHASE_1/API_SourceCode/App/api/controllers/ngoController.js
@@ -2,55 +2,95 @@ const ngoList = require('../../resources/ngo_list.json');
 const diseaseList = require('../../resources/disease_list.json');
 
 const getNGOs = (req, res) => {
-    var requestedLocations = req.query.locations.split(",");
-    var requestedDiseases = req.query.diseases.split(",");
+    var requestedLocations = [];
+    var requestedDiseases = [];
+    if (req.query.locations != undefined) {
+        requestedLocations = req.query.locations.split(",");
+    }
+    if (req.query.diseases != undefined) {
+        requestedDiseases = req.query.diseases.split(",");
+    }
 
     // Only look at valid requested diseases
-    var validDiseases = [];
-    requestedDiseases.forEach(requestedDisease => {
-        diseaseList.forEach(disease => {
-            if (disease.name.toLowerCase() === requestedDisease.toLowerCase()) {
-                validDiseases.push(requestedDisease);
-                return;
-            }
+    if (requestedDiseases.length > 0) {
+        var validDiseases = [];
+        requestedDiseases.forEach(requestedDisease => {
+            diseaseList.forEach(disease => {
+                console.log(disease.name, requestedDisease);
+                if (disease.name.toLowerCase() === requestedDisease.toLowerCase()) {
+                    validDiseases.push(requestedDisease);
+                    return;
+                }
+            })
         })
-    })
-    if (validDiseases.length <= 0) {
-        // No matches found
-        res.send([]);
-        return;
+        if (validDiseases.length <= 0) {
+            // No matches found
+            res.send([]);
+            return;
+        }
     }
+    console.log(requestedLocations, requestedDiseases);
 
     // Check for matching NGOs
     var matches = [];
-    ngoList.forEach((ngo) => {
-
-        // NGO must have both matching locations and diseases 
-
-        // if (ngo.keywords[0] === "all") {
-        //     // These NGOs match all diseases and should always be displayed
-        //     matches.push(ngo);
-        //     // Next iteration of the loop
-        //     return;
-        // }
-
-        // Check if NGO has matching locations
-        // for (let i = 0; i < requestedLocations.length; i++) {
-        //     if (ngo.locations.includes(requestedLocations[i].toLowerCase())) {
-        //         matches.push(ngo);
-        //         return;
-        //     }
-        // }
+    for (let k = 0; k < ngoList.length; k++) {
+        const ngo = ngoList[k];
 
 
-        // Check if NGO has matching diseases
-        // for (let i = 0; i < requestedDiseases.length; i++) {
-        //     if (ngo.keywords.includes(requestedDiseases[i].toLowerCase())) {
-        //         matches.push(ngo);
-        //         return;
-        //     }
-        // }
-    });
+        if (requestedLocations.length > 0 && requestedDiseases.length > 0) {
+            // NGO must have both matching locations and diseases 
+            var locationFound = false;
+            for (let i = 0; i < requestedLocations.length; i++) {
+                if (ngo.locations[0] === "all") {
+                    // These NGOs match all diseases and should always be displayed
+                    locationFound = true;
+                } else if (ngo.locations.includes(requestedLocations[i].toLowerCase())) {
+                    locationFound = true;
+                }
+            }
+
+            if (locationFound) {
+                for (let i = 0; i < requestedDiseases.length; i++) {
+                    if (ngo.keywords.includes(requestedDiseases[i].toLowerCase())) {
+                        matches.push(ngo);
+                    }
+                }
+            }
+
+        } else if (requestedLocations.length > 0) {
+            // Find all matching locations with any disease
+            for (let i = 0; i < requestedLocations.length; i++) {
+                if (ngo.locations[0] === "all") {
+                    // These NGOs match all locations and should always be displayed
+                    matches.push(ngo);
+                } else if (ngo.locations.includes(requestedLocations[i].toLowerCase())) {
+                    matches.push(ngo);
+                }
+            }
+
+        } else if (requestedDiseases.length > 0) {
+            // Find all matching diseases with any location 
+            for (let i = 0; i < requestedDiseases.length; i++) {
+                if (ngo.keywords[0] === "all") {
+                    // These NGOs match all diseases and should always be displayed
+                    matches.push(ngo);
+                } else if (ngo.keywords.includes(requestedDiseases[i].toLowerCase())) {
+                    matches.push(ngo);
+                }
+            }
+        }
+    }
+
+    // ngoList.forEach((ngo) => {
+
+    // if (ngo.keywords[0] === "all") {
+    //     // These NGOs match all diseases and should always be displayed
+    //     matches.push(ngo);
+    //     // Next iteration of the loop
+    //     return;
+    // }
+
+    // });
 
     res.send(matches);
 }
