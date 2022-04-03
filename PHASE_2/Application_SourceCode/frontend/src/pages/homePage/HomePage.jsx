1d50ff0bc6775c35acfbdfa57219ecb96d51dd1b
thao <reiin744@gmail.com>
2022-04-03 19:56:18 +1000

    Add styling to the front page

--- PHASE_2/Application_SourceCode/frontend/src/pages/homePage/HomePage.jsx
+++ PHASE_2/Application_SourceCode/frontend/src/pages/homePage/HomePage.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { FrontPageButton } from "../../components/frontPageButtons/FrontPageButton";
+import { GiveHelpButton, GetHelpButton } from "../../components/frontPageButtons/FrontPageButton";
+import BgGiveHelp from "../../assets/bggivehelp.jpg";
+import BgGetHelp from "../../assets/bggethelp.jpg";
+import "./homePage.css";
 
-export default function HomePage () {
+export default function HomePage() {
   return (
     <div className="HomePage">
-      <FrontPageButton buttonText="This is my button text" buttonOnClick={() => { console.log("Give help") }} />
+      <div className="left" style={{ backgroundImage: `url(${BgGiveHelp})` }}>
+          <div className="centered">
+              <GiveHelpButton buttonOnClick={() => { console.log("Give help") }} />
+              <h2>Give Help</h2>
+          </div>
+      </div>
+
+      <div className="right" style={{ backgroundImage: `url(${BgGetHelp})` }}>
+          <div className="centered">
+              <GetHelpButton buttonOnClick={() => { console.log("Get help") }} />
+              <h2>Get Help</h2>
+          </div>
+      </div>
     </div>
   );
 }
