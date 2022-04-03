7f736587f08e27d32bffaedda242d841f7bb3f06
thao <reiin744@gmail.com>
2022-04-03 19:57:47 +1000

    Change button to image

--- PHASE_2/Application_SourceCode/frontend/src/components/frontPageButtons/FrontPageButton.jsx
+++ PHASE_2/Application_SourceCode/frontend/src/components/frontPageButtons/FrontPageButton.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import "./frontPageButton.css"
+import "./frontPageButton.css";
+import GiveHelp from "../../assets/givehelp.jpg";
+import GetHelp from "../../assets/gethelp.jpg";
 
-export const FrontPageButton = ({ buttonText, buttonOnClick }) => {
+export const GiveHelpButton = ({ buttonOnClick }) => {
     return (
-        <button className="startButton" onClick={buttonOnClick}>
-            {buttonText}
+        <button className="startButton">
+            <img src={ GiveHelp } alt="Give Help" onClick={buttonOnClick}/>
         </button>
     );
 };
 
-FrontPageButton.propTypes = {
+export const GetHelpButton = ({ buttonOnClick }) => {
+    return (
+        <button className="startButton">
+            <img src={ GetHelp } alt="Give Help" onClick={buttonOnClick}/>
+        </button>
+    );
+};
+
+GiveHelpButton.propTypes = {
+    buttonText: PropTypes.string.isRequired,
+    buttonOnClick: PropTypes.func.isRequired
+};
+
+GetHelpButton.propTypes = {
     buttonText: PropTypes.string.isRequired,
     buttonOnClick: PropTypes.func.isRequired
 };
