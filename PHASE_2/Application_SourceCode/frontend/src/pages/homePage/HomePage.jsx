ffba689ba34a62a233b1cf54a49b042bbb2e54e9
thao <reiin744@gmail.com>
2022-04-04 20:54:04 +1000

    Remove import images

--- PHASE_2/Application_SourceCode/frontend/src/pages/homePage/HomePage.jsx
+++ PHASE_2/Application_SourceCode/frontend/src/pages/homePage/HomePage.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { GiveHelpButton, GetHelpButton } from "../../components/frontPageButtons/FrontPageButton";
-import BgGiveHelp from "../../assets/bggivehelp.jpg";
-import BgGetHelp from "../../assets/bggethelp.jpg";
 import "./homePage.css";
 
 export default function HomePage() {
   return (
     <div className="HomePage">
-      <div className="left" style={{ backgroundImage: `url(${BgGiveHelp})` }}>
+      <div className="left">
           <div className="centered">
               <GiveHelpButton buttonOnClick={() => { console.log("Give help") }} />
               <h2>Give Help</h2>
           </div>
       </div>
 
-      <div className="right" style={{ backgroundImage: `url(${BgGetHelp})` }}>
+      <div className="right">
           <div className="centered">
               <GetHelpButton buttonOnClick={() => { console.log("Get help") }} />
               <h2>Get Help</h2>
