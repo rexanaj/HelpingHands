146d6c28da6a84ad46f952eea7e471dcb76009b9
rexanaj <66309802+rexanaj@users.noreply.github.com>
2022-04-04 23:36:05 +1000

    Added twitter carousel to giveHelp page

--- PHASE_2/Application_SourceCode/frontend/src/app/App.js
+++ PHASE_2/Application_SourceCode/frontend/src/app/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 /////////// COMPONENTS /////////////
-import ExamplePage from "../pages/examplePage/ExamplePage.jsx";
-import TwitterApiPage from "../pages/twitterApiPage/TwitterApiPage";
 import GiveHelpPage from "../pages/giveHelpPage/GiveHelpPage.jsx"
 import HomePage from "../pages/homePage/HomePage.jsx";
 ////////////////////////////////////
@@ -16,8 +14,6 @@ export default function App () {
       <Routes>
         <Route exact path="/">
           <Route index element={<HomePage />} />
-          <Route path="/example" element={<ExamplePage word="DWEN" />} />
-          <Route path="/twitter" element={<TwitterApiPage />} />
           <Route path="/giveHelp" element={<GiveHelpPage word="DWEN" />} />
         </Route>
       </Routes>
--- PHASE_2/Application_SourceCode/frontend/src/pages/giveHelpPage/GiveHelpPage.jsx
+++ PHASE_2/Application_SourceCode/frontend/src/pages/giveHelpPage/GiveHelpPage.jsx
@@ -10,13 +10,14 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Spinner from 'react-bootstrap/Spinner';
 import Typography from '@mui/material/Typography';
 import "./GiveHelpPage.css"
+import { TwitterCarousel } from "../../components/twitterCarousel/TwitterCarousel";
 
 // This is an example of fetching from the api to get the sydnromes of a specific disease (in this case "Lassa Fever")
 // BTW, don't put your code in the pages, create seperate components and import those
-export default function GiveHelpPage(props) {
-    const [ disease, setDisease ] = useState('');
-    const [ articles, setArticles ] = useState([]);
-    const [ loading, setLoading ] = useState(false);
+export default function GiveHelpPage (props) {
+    const [disease, setDisease] = useState('');
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(false);
     const submitDisease = () => {
         // if (ingredientName === '' || ingredientName === null) {
         //     alert("Please select a disease")
@@ -25,12 +26,12 @@ export default function GiveHelpPage(props) {
         console.log("test")
         console.log(disease)
         fetchArticles();
-        
+
     };
-    
+
 
     const fetchArticles = async () => {
-        let formatDisease = disease.replace(/ /g,"%20")
+        let formatDisease = disease.replace(/ /g, "%20")
         const res = await fetch(`http://localhost:5555/articles?limit=4&keyterms=${formatDisease}`);
         setLoading(true);
         setArticles(await res.json());
@@ -52,7 +53,7 @@ export default function GiveHelpPage(props) {
         let month = String(pub_date.getMonth() + 1);
         let day = String(pub_date.getDate());
         const year = String(pub_date.getFullYear());
-      
+
         if (month.length < 2) month = '0' + month;
         if (day.length < 2) day = '0' + day;
 
@@ -70,10 +71,10 @@ export default function GiveHelpPage(props) {
         );
         i += 1;
     }
-  return (
-    <div>
-        <div className="searchBar">
-            <Autocomplete
+    return (
+        <div>
+            <div className="searchBar">
+                <Autocomplete
                     disablePortal
                     id="search"
                     value={disease}
@@ -81,31 +82,33 @@ export default function GiveHelpPage(props) {
                     onChange={(event, value) => setDisease(value)}
                     sx={{ width: 300 }}
                     renderInput={(params) => <TextField {...params} label="Select your disease" />}
-            />
-            <Button id="searchButton" variant="contained" onClick={submitDisease}>Search For Disease</Button>
-        </div>
-        
-        <div>
-            {loading ? 
+                />
+                <Button id="searchButton" variant="contained" onClick={submitDisease}>Search For Disease</Button>
+            </div>
+
             <div>
-                <Typography className="heading" variant="h5" component="body" gutterBottom>
-                    WHO Articles
-                </Typography>
-                <Grid container spacing={3}>{articles_holder}</Grid>
-            </div> : (
-                    <Button className="spinner" variant="success" disabled>
-                        <Spinner
-                        as="span"
-                        animation="grow"
-                        size="sm"
-                        role="status"
-                        aria-hidden="true"
-                        />
-                        Please enter a disease
-                    </Button>
-            )}
+                {loading ?
+                    <div>
+                        <Typography className="heading" variant="h5" component="body" gutterBottom>
+                            WHO Articles
+                        </Typography>
+                        <Grid container spacing={3}>{articles_holder}</Grid>
+                        <TwitterCarousel keyword={disease} />
+                    </div> : (
+                        <Button className="spinner" variant="success" disabled>
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            Please enter a disease
+                        </Button>
+                    )}
+            </div>
+
         </div>
-    </div>
-  );
+    );
 }
 
