cc5ca40999d1673359612d66b3248e8b7960296a
Jared Smith <61144856+MrBanananator@users.noreply.github.com>
2022-04-05 16:56:25 +1000

    Link cards with giveHelp search page (#27)
    Co-authored-by: rexanaj <66309802+rexanaj@users.noreply.github.com>

--- PHASE_2/Application_SourceCode/frontend/src/app/App.js
+++ PHASE_2/Application_SourceCode/frontend/src/app/App.js
@@ -4,10 +4,9 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 /////////// COMPONENTS /////////////
-import HomePage from "../pages/home/HomePage";
+import HomePage from "../pages/homePage/HomePage";
 import { Header } from "../components/header/Header";
-import NgoCardsGrid from "../components/NgoCardsGrid/NgoCardsGrid.jsx";
-import GiveHelpPage from "../pages/giveHelpPage/GiveHelpPage.jsx"
+import GiveHelpPage from "../pages/giveHelpPage/GiveHelpPage.jsx";
 import GetHelpPage from "../pages/getHelpPage/GetHelpPage";
 ////////////////////////////////////
 
@@ -20,7 +19,6 @@ export default function App () {
           <Route index element={<HomePage />} />
           <Route path="/giveHelp" element={<GiveHelpPage />} />
           <Route path="/getHelp" element={<GetHelpPage />} />
-          <Route path="/ngos" element={<NgoCardsGrid />} />
         </Route >
       </Routes >
     </div >
