b93390c7ed93f614254643731d20f10211e1135c
Jared Smith <61144856+MrBanananator@users.noreply.github.com>
2022-05-17 19:57:41 +1000

    UI changes

--- PHASE_2/Application_SourceCode/frontend/src/pages/getHelpPage/GetHelpPage.jsx
+++ PHASE_2/Application_SourceCode/frontend/src/pages/getHelpPage/GetHelpPage.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import options from "../../components/Diseases"
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import options from "../../components/Diseases";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 import { Modal } from "@mui/material";
 import "./GetHelpPage.css";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Checkbox, FormControlLabel } from "@mui/material";
 import img1 from "../../assets/img/img1.png";
 // import img2 from "../../assets/img/img2.jpg";
 // import img3 from "../../assets/img/img3.jpeg";
-import addIcon from "../../assets/img/add-icon.png"
-import callIcon from "../../assets/img/call-icon.png"
-import emailIcon from "../../assets/img/Message_alt_fill.png"
+import addIcon from "../../assets/img/add-icon.png";
+import callIcon from "../../assets/img/call-icon.png";
+import emailIcon from "../../assets/img/Message_alt_fill.png";
 import { Avatar } from "@mui/material";
 
-export default function GetHelpPage () {
+export default function GetHelpPage() {
   const [loading, setLoading] = useState(false);
-  const [name, setName] = useState('');
-  const [advice, setAdvice] = useState('');
-  const [disease, setDisease] = useState('');
+  const [name, setName] = useState("");
+  const [advice, setAdvice] = useState("");
+  const [disease, setDisease] = useState("");
 
   // advice cards
   const [responses, setResponses] = useState([]);
@@ -40,33 +40,32 @@ export default function GetHelpPage () {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 700,
-    bgcolor: 'background.paper',
+    bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
-  }
-
+  };
 
   const getAdvice = async () => {
     const res = await fetch(`http://localhost:5555/posts/${disease}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
     });
 
     const responses = await res.json();
     setResponses(responses);
     setLoading(true);
-  }
+  };
 
   // job board is updated here
   const submitDisease = () => {
-    console.log("test")
+    console.log("test");
     setLoading(false);
 
     // update the advice section
@@ -90,20 +89,20 @@ export default function GetHelpPage () {
     }
 
     const res = await fetch(`http://localhost:5555/posts/makePost`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
         name: name,
         content: advice,
         disease: disease,
-      })
+      }),
     });
 
     const body = await res.json();
     if (body.error) {
-      alert(body.error)
+      alert(body.error);
     } else {
       toast(`Advice from ${name} added successfully!`);
       setAdvice("");
@@ -111,8 +110,7 @@ export default function GetHelpPage () {
       setCheck1(false);
       setCheck2(false);
     }
-  }
-
+  };
 
   return (
     <div>
@@ -131,7 +129,13 @@ export default function GetHelpPage () {
               >
                 Find Help Here
               </Typography>
-              <Typography variant="h5" align="center" color="text.secondary" paragraph >
+              <Typography
+                variant="h5"
+                align="center"
+                color="text.secondary"
+                paragraph
+                id="gethelp-subtitle"
+              >
                 See what others in your situation are saying.
               </Typography>
               <Stack
@@ -145,11 +149,21 @@ export default function GetHelpPage () {
                   id="search"
                   value={disease}
                   options={options}
-                  onChange={(event, value) => { setDisease(value) }}
+                  onChange={(event, value) => {
+                    setDisease(value);
+                  }}
                   sx={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Select a disease" />}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Select a disease" />
+                  )}
                 />
-                <Button id="searchButton" variant="contained" onClick={submitDisease}>Get help</Button>
+                <Button
+                  id="searchButton"
+                  variant="contained"
+                  onClick={submitDisease}
+                >
+                  Get help
+                </Button>
               </Stack>
             </Container>
           </Box>
@@ -160,50 +174,72 @@ export default function GetHelpPage () {
       <ToastContainer />
 
       <ToastContainer />
-      <div className='government-hotline'>
-        <div className='gov-box'>
-          <div className='gov-name'>World Health Organisation</div>
-          <div className='gov-body'>
-            <div className="icon-text"><img src={callIcon}></img>Call +41 22 791 21 11</div>
-            <div className="icon-text"><img src={emailIcon}></img>https://www.who.int/about/contact-us</div>
+      <div className="government-hotline">
+        <div className="gov-box">
+          <div className="gov-name">World Health Organisation</div>
+          <div className="gov-body">
+            <div className="icon-text">
+              <img src={callIcon}></img> +41 22 791 21 11
+            </div>
+            <div className="icon-text">
+              <img src={emailIcon}></img> https://www.who.int/about/contact-us
+            </div>
           </div>
         </div>
-        <div className='gov-box'>
-          <div className='gov-name'>The Center for Disease Control and Prevention</div>
-          <div className='gov-body'>
-            <div className="icon-text"><img src={callIcon}></img>Call 800 232 4636</div>
-            <div className="icon-text"><img src={emailIcon}></img>https://wwwn.cdc.gov/DCS/ContactUs/Form</div>
+        <div className="gov-box">
+          <div className="gov-name">
+            The Center for Disease Control and Prevention
+          </div>
+          <div className="gov-body">
+            <div className="icon-text">
+              <img src={callIcon}></img> 1800 232 4636
+            </div>
+            <div className="icon-text">
+              <img src={emailIcon}></img>{" "}
+              https://wwwn.cdc.gov/DCS/ContactUs/Form
+            </div>
           </div>
         </div>
-        <div className='gov-box'>
-          <div className='gov-name'>Doctors Without Borders</div>
-          <div className='gov-body'>
-            <div className="icon-text"><img src={callIcon}></img>Call 2 8570 2600</div>
-            <div className="icon-text"><img src={emailIcon}></img>https://www.msf.org/contact-us</div>
+        <div className="gov-box">
+          <div className="gov-name">Doctors Without Borders</div>
+          <div className="gov-body">
+            <div className="icon-text">
+              <img src={callIcon}></img> 02 8570 2600
+            </div>
+            <div className="icon-text">
+              <img src={emailIcon}></img> https://www.msf.org/contact-us
+            </div>
           </div>
         </div>
       </div>
-      {
-        loading ? <Container sx={{ py: 8 }} maxWidth="md" id="gethelp-card-container">
-          {/* End hero unit */}
+      {loading ? (
+        <Container sx={{ py: 8 }} id="gethelp-card-container">
           <h1 className="gethelp-header">Advice from our users</h1>
-          {responses.length > 0 ? <Grid id="card-list" container spacing={4}>
-            {responses.map((card, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4} className="card-list">
-                <div className="card">
-                  <div className="header">
-                    <h2>{card.name}</h2>
-                  </div>
-                  <div className="content">
-                    <p className="quotation-mark">&ldquo;</p>
-                    <div className="quote">
-                      <p>{card.content}</p>
+          {responses.length > 0 ? (
+            <Grid id="card-list" container spacing={4}>
+              {responses.map((card, index) => (
+                <Grid
+                  item
+                  key={index}
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  className="card-list"
+                >
+                  <div className="card">
+                    <div className="header">
+                      <h2>{card.name}</h2>
                     </div>
-                    <p className="quotation-mark end">&rdquo;</p>
-                    <button>Contact</button>
+                    <div className="content">
+                      <p className="quotation-mark">&ldquo;</p>
+                      <div className="quote">
+                        <p>{card.content}</p>
+                      </div>
+                      <p className="quotation-mark end">&rdquo;</p>
+                      <button>Contact</button>
+                    </div>
                   </div>
-                </div>
-                {/* <Card
+                  {/* <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
                   <CardContent sx={{ flexGrow: 1 }}>
@@ -218,11 +254,19 @@ export default function GetHelpPage () {
                     <Button size="small">Contact</Button>
                   </CardActions>
                 </Card> */}
-              </Grid>
-            ))}
-          </Grid> : <h1>No posts yet</h1>
-          }
-          <Button id="advice-button" startIcon={<Avatar src={addIcon} id="icon"/>} onClick={handleOpen}>Add advice</Button>
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <h1>No posts yet</h1>
+          )}
+          <Button
+            id="advice-button"
+            startIcon={<Avatar src={addIcon} id="icon" />}
+            onClick={handleOpen}
+          >
+            Add advice
+          </Button>
           <Modal
             open={open}
             onClose={handleClose}
@@ -230,7 +274,9 @@ export default function GetHelpPage () {
             aria-describedby="modal-modal-description"
           >
             <Box sx={style}>
-              <h1 className="gethelp-header">Add some advice for anybody affected</h1>
+              <h1 className="gethelp-header">
+                Add some advice for anybody affected
+              </h1>
               <div id="enterForm">
                 <h2 className="subtitle">Enter your name</h2>
                 <TextField
@@ -239,7 +285,9 @@ export default function GetHelpPage () {
                   id="outlined-textarea"
                   label="Username"
                   placeholder="Enter your name"
-                  onChange={(e) => { setName(e.target.value) }}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
                 <h2 className="subtitle">Enter some advice</h2>
                 <TextField
@@ -249,23 +297,50 @@ export default function GetHelpPage () {
                   id="outlined-multiline-static"
                   rows={4}
                   label="add some advice"
-                  onChange={(e) => { setAdvice(e.target.value) }}
+                  onChange={(e) => {
+                    setAdvice(e.target.value);
+                  }}
                 />
                 <div id="checkbox-area">
-                  <FormControlLabel className="checkbox" control={<Checkbox checked={check1} onChange={(v) => setCheck1(v.target.checked)} />} label="I acknowledge that all messages on this site are posted in good faith." />
-                  <FormControlLabel className="checkbox" control={<Checkbox checked={check2} onChange={(v) => setCheck2(v.target.checked)} />} label="This message contains truthful information." />
+                  <FormControlLabel
+                    className="checkbox"
+                    control={
+                      <Checkbox
+                        checked={check1}
+                        onChange={(v) => setCheck1(v.target.checked)}
+                      />
+                    }
+                    label="I acknowledge that all messages on this site are posted in good faith."
+                  />
+                  <FormControlLabel
+                    className="checkbox"
+                    control={
+                      <Checkbox
+                        checked={check2}
+                        onChange={(v) => setCheck2(v.target.checked)}
+                      />
+                    }
+                    label="This message contains truthful information."
+                  />
                 </div>
-              <Button variant="contained" onClick={addAdvice} id="adviceButton">Add advice</Button>
+                <Button
+                  variant="contained"
+                  onClick={addAdvice}
+                  id="adviceButton"
+                >
+                  Add advice
+                </Button>
               </div>
             </Box>
           </Modal>
-        </Container> : <div></div>
-      }
-
+        </Container>
+      ) : (
+        <div></div>
+      )}
 
       {/* Footer */}
 
       {/* End footer */}
-    </div >
+    </div>
   );
 }
